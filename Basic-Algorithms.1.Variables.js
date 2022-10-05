2586ba2189a869f10eb4d3eb6d44a05fa0c952bb
Raul <raul2851@gmail.com>
2022-10-05 18:33:43 +0200

    ejercicios

---
+++ Basic-Algorithms.1.Variables.js
@@ -0,0 +1,18 @@
+// 1: Variables
+
+// 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+
+const myFavoriteHero = "Hulk"
+
+//1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+
+var x = 50;
+
+//1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
+
+var h = 5;
+var y = 10;
+
+//1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+
+var z = h + y;
---
+++ Basic-Algorithms.3.Operadores.js
@@ -0,0 +1,32 @@
+// 3: Operadores
+
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+
+var multiplica = 10 * 5;
+console.log(multiplica);
+
+//1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+var divide = 10 / 2;
+console.log(divide);
+
+//1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+var resto = 15 % 9;
+console.log(resto);
+
+//1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+//teniendo dos variables y = 10 y z = 5.
+
+var y = 10;
+var z = 5;
+var x = y + z;
+console.log(x);
+
+//1.5 Usa el correcto operador de asignación que resultará en x = 50,
+//teniendo dos variables y = 10 y z = 5.
+
+var y = 10;
+var z = 5;
+var x = y * z;
+console.log(x);
